56ac990195307f19209a3fe86418153def411402
JacobiColeman <112585897+JacobiColeman@users.noreply.github.com>
2023-09-29 17:22:24 -0400

    Bird progress (infinite loop)

--- Assets/Code/Environment/jc.cs
+++ Assets/Code/Environment/jc.cs
@@ -7,19 +7,19 @@ public class jc : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3 (0, 10, 0);
+        transform.position += new Vector3 (Camera.main.transform.position.x - 15,0 ,0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < 100)
+        if (transform.position.x < Camera.main.transform.position.x + 15)
         {
             transform.position += new Vector3 (10,0,0) * Time.deltaTime;
         } 
         else
         {
-            transform.position = new Vector3 (0,10,0);
+            transform.position = new Vector3 (Camera.main.transform.position.x - 15,0 ,0);
         }
 
     }
