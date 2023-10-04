a72332e35f8155902e8bddf913c71c69a98a4502
JacobiColeman <112585897+JacobiColeman@users.noreply.github.com>
2023-10-04 19:55:35 -0400

    Fixed randomized sprite(bird) spawn
    Fixed the randomized y positioning and set a wait time for when the "bird" flies across the screen

--- Assets/Code/Environment/jc.cs
+++ Assets/Code/Environment/jc.cs
@@ -11,6 +11,9 @@ public class jc : MonoBehaviour
     }
 
     // Update is called once per frame
+    float resetTimer;
+    float timer;
+    
     void Update()
     {
         if (transform.position.x < Camera.main.transform.position.x + 15)
@@ -19,7 +22,14 @@ public class jc : MonoBehaviour
         } 
         else
         {
-            transform.position = new Vector3 (Camera.main.transform.position.x - 15,0 ,0);
+            timer+= Time.deltaTime;
+            if (timer > resetTimer){
+                timer = 0;
+                resetTimer = Random.Range(4, 15);
+                transform.position = new Vector3 (Camera.main.transform.position.x - 15,Camera.main.transform.position.y + Random.Range(-8,8) ,0);
+            }
+            // transform.position = new Vector3 (Camera.main.transform.position.x - 15,0 ,0);
+            
         }
 
     }
